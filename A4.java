5ccb39ea50869cfedab5552fc7717b5d6aa81900
nagarjundhar-10101996 <142318278+nagarjundhar-10101996@users.noreply.github.com>
2023-11-05 14:53:53 +0530

    Update A4.java
    Change the class name and description

--- A4.java
+++ A4.java
@@ -1,21 +1,22 @@
 class A4 {
     public static void main(String[] args) throws InterruptedException {
         String course[] ={"Java", "J2EE", "Spring"};
-        InnerThreadon n = new InnerThreadon(course);
-        InnerThreadonchar t = new InnerThreadonchar(course);
+        Course n = new Course(course);
+        Courseext t = new Courseext(course);
+        Course d = new Course(course);
+        
         n.start();
         t.start();
-        n.join();
-        t.join();
+        d.start();
        
     }
 }
 /**
  * InnerThreadon
  */
- class InnerThreadon extends Thread {
+ class Course extends Thread {
     private String [] n;
-    public InnerThreadon(String[] course) {
+    public Course(String[] course) {
         this.n = course;
     }
     public void run()
@@ -23,17 +24,15 @@ class A4 {
         for (int i = 0; i < this.n.length; i++) {
             System.out.printf("Thread %d : %s\n" , i+1 , n[i]);
         }
-        
-        
     }
     
 }
 /**
  * InnerThreadon
  */
- class InnerThreadonchar extends Thread {
+ class Courseext extends Thread {
     private String [] k;
-    public InnerThreadonchar(String[] course) {this.k = course;}
+    public Courseext(String[] course) {this.k = course;}
 
     public void run()
     {
